24c5ccfea62b6ea85904afae127059df30041a96
sanjay <sanjayr2206@gmail.com>
2017-06-06 19:48:33 +0530

    index

--- salesforceconnect.js
+++ salesforceconnect.js
@@ -4,49 +4,38 @@ var pg = require('pg');
 //var Promise = require('promise');
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
+//client.connect();
 
 //var client = new pg.Client(process.env.DATABASE_URL);
 
-module.exports = function(){
-
-        /*client.connect(function(err) {
-            if (err) {
-                return err;
-            }
+function getData(back){
             
-        };
-        var query = client.query(
-                    'SELECT firstname,lastname,email FROM salesforce.Lead');
-        query.on("row", function (row, result) {
-            result.addRow(row);
-        });
+    pg.connect(process.env.DATABASE_URL , function (err,client,done) {
+        if (err) {
+            back(err);
 
-        query.on("end",function(result){
-            console.log(result);
-            return 'data';
-            
-        });*/
-        return pg.connect(process.env.DATABASE_URL , function (err,client,done) {
-            if (err) {
-                return err;
-            }
+        }else{
             console.log('Connected to postgres! Getting schemas...');
-            
+        
             client.query(
                 'SELECT firstname,lastname,email FROM salesforce.Lead',
                 function(err, result) {
                     if(err){
-                       return err;
+                       back(err);
                     }
                     done();
-                    return result.rows[0].firstname;
+                    back(result.rows[0].firstname);
                     //done(); 
                     //return result.rows[0].firstname;
                 }
-            );
-
-        });
-
-        //return 'data1';
+            );    
+        }
         
-    }
+
+    });
+}
+
+module.exports = getData(function(data){
+    if(data != undefined) return data;
+    return 'No result';
+})
