17cab8e4547cbe3a47098e36503e4ba91d4c2a3c
sanjay <sanjayr2206@gmail.com>
2017-06-06 19:28:46 +0530

    index'

--- salesforceconnect.js
+++ salesforceconnect.js
@@ -5,9 +5,16 @@ var pg = require('pg');
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 
-var client = new pg.Client(process.env.DATABASE_URL);
+//var client = new pg.Client(process.env.DATABASE_URL);
 
 module.exports = function(){
+
+        /*client.connect(function(err) {
+            if (err) {
+                return err;
+            }
+            
+        };
         var query = client.query(
                     'SELECT firstname,lastname,email FROM salesforce.Lead');
         query.on("row", function (row, result) {
@@ -18,8 +25,28 @@ module.exports = function(){
             console.log(result);
             return 'data';
             
+        });*/
+        return pg.connect(process.env.DATABASE_URL , function (err,client,done) {
+            if (err) {
+                return err;
+            }
+            console.log('Connected to postgres! Getting schemas...');
+            
+            client.query(
+                'SELECT firstname,lastname,email FROM salesforce.Lead',
+                function(err, result) {
+                    if(err){
+                       return err;
+                    }
+                    done();
+                    return result.rows[0].firstname;
+                    //done(); 
+                    //return result.rows[0].firstname;
+                }
+            );
+
         });
 
-        return 'data1';
+        //return 'data1';
         
     }
