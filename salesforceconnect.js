37bfbe4a0ce5d7b4a4ae5d714ffc6cb173078661
sanjay <sanjayr2206@gmail.com>
2017-06-06 13:37:48 +0530

    index

--- salesforceconnect.js
+++ salesforceconnect.js
@@ -4,28 +4,30 @@ var pg = require('pg');
 var Promise = require('promise');
 //var pgClient = new pg.Client(process.env.DATABASE_URL);
 
-
- 
-
-module.exports = function(){
-    return pg.connect(process.env.DATABASE_URL).spread( function (client,done) {
-            /*if (err) {
-              console.log("not able to get connection "+ err);
-            }*/
+function getData(){
+            
+    pg.connect(process.env.DATABASE_URL , function (client,done) {
+        if (err) {
+            return console.log("not able to get connection "+ err);
+        }
         console.log('Connected to postgres! Getting schemas...');
         
-        var query = client.query({
-            text: "SELECT firstname,lastname,email FROM salesforce.Lead",
-            buffer: true 
-        }).promise.then(
-            function(result) {
-              
-                  console.log(result.rows[0].firstname);
-                  return result.rows[0].firstname;
-                  
-          },
-          function(err) { throw err; }
-        ).finally(done);
-        
-    }).done();  
+        client.query(
+            'SELECT firstname,lastname,email FROM salesforce.Lead',
+            function(err, result) {
+                if(err){
+                   console.log(err);
+                }
+                done();
+                return result.rows[0].firstname;
+                //done(); 
+                //return result.rows[0].firstname;
+            }
+        );
+
+    });
 }
+
+
+module.exports = getData;
+    
